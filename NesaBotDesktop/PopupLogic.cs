15b395b8b2feff5ec63bb9b6324987b22919467a
Nico Nussmüller <borelio836@gmail.com>
2022-06-09 21:11:59 +0200

    feat: popup logic

---
+++ NesaBotDesktop/PopupLogic.cs
@@ -0,0 +1,15 @@
+﻿namespace NesaBotDesktop {
+  public static class PopupLogic {
+    public static void ShowErrorMessage(string message, string title = "") {
+      MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    public static void ShowWarningMessage(string message, string title = "") {
+      MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    }
+
+    public static void ShowInfoMessage(string message, string title = "") {
+      MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+  }
+}
